b950e557951b42ceaa10b05d4133849bfd9cc35b
Tainá Kusuo Savino <99339654+tatasavino@users.noreply.github.com>
2022-06-11 12:38:24 -0300

    Add files via upload
    p3

---
+++ Funcionario.cs
@@ -0,0 +1,55 @@
+using System;
+public class Funcionario
+{
+    string cpf;
+    string nome;
+    string sobrenome;
+    double salario;
+
+    public Funcionario (string cpf, string nome, string sobrenome, double salario)
+    {
+        this.cpf = cpf;
+        this.nome = nome;
+        this.sobrenome = sobrenome;
+        this.salario = salario;
+    }
+
+    public override string ToString () 
+    {
+        return $"\n\n\tNome: {this.nome} {this.sobrenome}\n\tCpf: {this.cpf}\n\tSalário: {this.salario}\n";
+    }
+
+    public string GetCpf () 
+    {
+        return this.cpf;
+    }
+    public string GetNome () 
+    {
+        return this.nome;
+    }
+    public string GetSobrenome () 
+    {
+        return this.sobrenome;
+    }
+    public double GetSalario () 
+    {
+        return this.salario;
+    }
+
+    public double GetSalarioAnual ()
+    {
+        return (this.salario*12);
+    }
+
+    public void SetSalario (double salario) 
+    {
+        this.salario = salario;
+    }
+    public void AumentarSalario (double percentual)
+    {
+        percentual = (percentual/100);
+        this.salario = (this.salario + this.salario*percentual);
+
+    }
+
+}
---
+++ Program.cs
@@ -0,0 +1,65 @@
+﻿using System;
+
+namespace p3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int opcao = 0;
+            string cpf="", nome="", sobrenome="";
+            double salario=0.0;
+
+            List <Funcionario> funcionario = new List<Funcionario>();
+            Program program = new Program();
+
+            
+            do 
+            {
+                program.GerarMenu();
+                opcao = Convert.ToInt32(Console.ReadLine());
+                switch  (opcao)
+                {
+                    case 1: 
+                        program.AdicionarFuncionario(ref cpf, ref nome, ref sobrenome, ref salario);
+                        Funcionario f1 = new Funcionario(cpf, nome, sobrenome, salario);
+                        funcionario.Add(f1);
+                        break;
+                    case 2:
+                        Console.WriteLine("Programa Encerrado.\n\n");
+                        break;
+                    default:
+                        Console.WriteLine("\nOpção inválida. Tente novamente.");
+                        break;
+                }
+
+            } while (opcao !=4);
+
+
+            
+
+
+        }
+
+        public void GerarMenu() 
+        {
+            Console.Write($"\n\nOlá! Seja bem vindo(a). Escolha uma opção: \n\n\t1 - Adicionar funcionário;\t2 - Encerrar Programa;\n\nEscolha: ");
+        }
+
+        public void AdicionarFuncionario (ref string cpf, ref string nome, ref string sobrenome, ref double salario) 
+        {
+            Console.WriteLine("\n--------------------------------------------------------------------------------\n\n");
+            Console.Write("Cpf: ");
+            cpf = Console.ReadLine();
+            Console.Write("Nome: ");
+            nome = Console.ReadLine();
+            Console.Write("Sobrenome: ");
+            sobrenome = Console.ReadLine();
+            Console.Write("Salário: ");
+            salario = Double.Parse(Console.ReadLine());
+            Console.WriteLine("\n--------------------------------------------------------------------------------");
+        }
+
+
+    }
+}
